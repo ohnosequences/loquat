2d2fe214bc88a917258aed876ac13875138ed207
Alexey Alekhin <laughedelic@gmail.com>
2015-11-04 18:12:36 +0100

    Updated test code example

--- src/test/scala/ohnosequences/loquat/dataMappings.scala
+++ src/test/scala/ohnosequences/loquat/dataMappings.scala
@@ -2,21 +2,22 @@ package ohnosequences.loquat.test
 
 object dataMappingsExample {
 
-  // import ohnosequences.awstools.s3._
-  // import ohnosequences.loquat._, dataMappings._
-  // import ohnosequences.datasets._, s3Locations._
-  // import ohnosequences.cosas._, types._, typeSets._, properties._, records._
-  // import instructionsExample._
-  //
-  // 
-  // val dataMapping = DataMapping(id = "dataMapping3498734", instructions = instructs)(
-  //   remoteInput =
-  //     sample.atS3(ObjectAddress("", "")) :~:
-  //     fastq.atS3(ObjectAddress("", "")) :~:
-  //     ∅,
-  //   remoteOutput =
-  //     stats.atS3(ObjectAddress("", "")) :~:
-  //     results.atS3(ObjectAddress("", "")) :~:
-  //     ∅
-  // )
+  import ohnosequences.awstools.s3._
+  import ohnosequences.loquat._, dataMappings._
+  import ohnosequences.datasets._, s3Locations._
+  import ohnosequences.cosas._, typeSets._
+  import instructionsExample._
+
+
+  val dataMapping = DataMapping(id = "dataMapping3498734", dataProcessing = instructs)(
+    remoteInput =
+      sample.inS3(S3Object("bucket", "key")) :~:
+      fastq.inS3(S3Object("bucket", "key")) :~:
+      ∅,
+    remoteOutput =
+      stats.inS3(S3Object("bucket", "key")) :~:
+      results.inS3(S3Object("bucket", "key")) :~:
+      ∅
+  )
+
 }
--- src/test/scala/ohnosequences/loquat/instructions.scala
+++ src/test/scala/ohnosequences/loquat/instructions.scala
@@ -2,41 +2,39 @@ package ohnosequences.loquat.test
 
 object instructionsExample {
 
-  // import ohnosequences.loquat._, dataMappings._, dataProcessing._
-  // import ohnosequences.statika.instructions._
-  // import ohnosequences.datasets._, dataSets._, fileLocations._
-  // import ohnosequences.cosas._, types._, typeSets._, properties._, records._
-  // import java.io.File
-  //
-  //
-  // // inputs:
-  // case object SomeData extends AnyDataType { val label = "someLabel" }
-  // case object sample extends Data(SomeData, "sample")
-  // case object fastq extends Data(SomeData, "fastq")
-  //
-  // // outputs:
-  // case object stats extends Data(SomeData, "stats")
-  // case object results extends Data(SomeData, "results")
-  // 
-  // FIXME: denotation parsers
+  import ohnosequences.loquat._, dataProcessing._
+  import ohnosequences.statika.instructions._
+  import ohnosequences.datasets._, dataSets._, fileLocations._
+  import ohnosequences.cosas._, typeSets._
+  import java.io.File
+
+
+  // inputs:
+  case object SomeData extends AnyDataType { val label = "someLabel" }
+  case object sample extends Data(SomeData, "sample")
+  case object fastq extends Data(SomeData, "fastq")
+
+  // outputs:
+  case object stats extends Data(SomeData, "stats")
+  case object results extends Data(SomeData, "results")
 
   // instructions:
-  // case object instructs extends DataProcessingBundle()(
-  //   input = sample :^: fastq :^: DNil,
-  //   output = stats :^: results :^: DNil
-  // ) {
-  //
-  //   def install: Results = say("horay!")
-  //
-  //   def processDataMapping(dataMappingId: String, workingDir: File): (Results, OutputFiles) = {
-  //     val files =
-  //       stats.inFile(new File(dataMappingId)) :~:
-  //       results.inFile(new File("")) :~:
-  //       ∅
-  //     (say("foo"), files)
-  //   }
-  // }
-  //
+  case object instructs extends DataProcessingBundle()(
+    input = sample :^: fastq :^: DNil,
+    output = stats :^: results :^: DNil
+  ) {
+
+    def install: AnyInstructions = say("horay!")
+
+    def processDataMapping(dataMappingId: String, workingDir: File): Instructions[OutputFiles] = {
+      success("foo",
+        stats.inFile(new File(dataMappingId)) :~:
+        results.inFile(new File(".")) :~:
+        ∅
+      )
+    }
+  }
+
   // val outputs = instructs.processDataMapping("foo", new File("."))._2
 
   // NOTE: here we can use Map.apply safely, because we know
