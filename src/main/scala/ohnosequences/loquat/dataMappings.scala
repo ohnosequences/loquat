d2bb38e0b0c79c6842a6c3792dd3c1463d20d3f1
Alexey Alekhin <laughedelic@gmail.com>
2016-02-18 20:12:21 +0100

    Added a check for missing/non-needed input/output data keys in the datamappings

--- src/main/scala/ohnosequences/loquat/dataMappings.scala
+++ src/main/scala/ohnosequences/loquat/dataMappings.scala
@@ -11,7 +11,7 @@ import better.files._
 import upickle.Js
 
 
-trait AnyDataMapping {
+trait AnyDataMapping { dataMapping =>
 
   // This label is just to distinguish data mappings, it is not an ID
   val label: String
@@ -25,6 +25,38 @@ trait AnyDataMapping {
 
   type RemoteOutput = Map[AnyData, S3Resource]
   val  remoteOutput: RemoteOutput
+
+  def checkDataKeys: Seq[String] = {
+
+    def keyLabels(rec: AnyRecordType): Set[String] =
+      rec.keys.types.asList.toSet.map { tpe: AnyType => tpe.label }
+
+    val mapInputKeys = remoteInput.keySet.map{ _.label }
+    val dataInputKeys = keyLabels(dataProcessing.input)
+
+    val missingInputKeys: Set[String] = dataInputKeys diff mapInputKeys
+    val extraInputKeys:   Set[String] = mapInputKeys diff dataInputKeys
+
+
+    val mapOutputKeys = remoteOutput.keySet.map{ _.label }
+    val dataOutputKeys = keyLabels(dataProcessing.output)
+
+    val missingOutputKeys: Set[String] = dataOutputKeys diff mapOutputKeys
+    val extraOutputKeys:   Set[String] = mapOutputKeys diff dataOutputKeys
+
+    missingInputKeys.toSeq.map { key =>
+      s"The [${key}] input key is missing in the remoteInput of [${dataMapping.label}]"
+    } ++
+    extraInputKeys.toSeq.map { key =>
+      s"The [${key}] key doesn't exist in the [${dataMapping.label}] input dataset"
+    } ++
+    missingOutputKeys.toSeq.map { key =>
+      s"The [${key}] output key is missing in the remoteOutput of [${dataMapping.label}]"
+    } ++
+    extraOutputKeys.toSeq.map { key =>
+      s"The [${key}] key doesn't exist in the [${dataMapping.label}] output dataset"
+    }
+  }
 }
 
 case class DataMapping[DP <: AnyDataProcessingBundle](
