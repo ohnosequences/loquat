5b33d570b5a3bf9d09780d46c98b8b6a92751699
Alexey Alekhin <laughedelic@gmail.com>
2017-09-07 04:55:00 +0200

    Improved worker logging

--- src/main/scala/ohnosequences/loquat/worker.scala
+++ src/main/scala/ohnosequences/loquat/worker.scala
@@ -146,14 +146,13 @@ class DataProcessor(
   ): AnyResult = {
 
     try {
-
-      logger.info("Preparing dataMapping input")
+      logger.debug("Preparing dataMapping input")
 
       val inputDir = (workingDir / "input").createDirectories()
 
       val inputFiles: Map[String, File] = dataMapping.inputs.map { case (name, resource) =>
 
-        logger.debug(s"Trying to create input object [${name}]")
+        logger.debug(s"Trying to create input object [${name}] from \n${resource}")
 
         resource match {
           case MessageResource(msg) => {
@@ -172,7 +171,7 @@ class DataProcessor(
         }
       }
 
-      logger.info("Processing data in: " + workingDir.path)
+      // logger.debug("Processing data in: " + workingDir.path)
       val result = instructionsBundle.runProcess(workingDir, inputFiles)
 
       val resultDescription = ProcessingResult(dataMapping.id, result.toString)
@@ -261,15 +260,15 @@ class DataProcessor(
       try {
         val transferManager = aws.s3.createTransferManager
 
-        logger.info("Data processor is waiting for new data")
+        logger.info("Waiting for new data")
 
         val message = waitForTask()
 
         // instance.foreach(_.createTag(StatusTag.processing))
-        logger.info("DataProcessor: received message " + message)
         val dataMapping = SimpleDataMapping.deserialize(message.body)
 
-        logger.info("DataProcessor processing message")
+        logger.info(s"Processing message [${dataMapping.id}]")
+
         import scala.concurrent.ExecutionContext.Implicits._
         val futureResult = Future {
           processDataMapping(transferManager, dataMapping, workingDir)
