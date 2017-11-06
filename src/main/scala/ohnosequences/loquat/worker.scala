690f530d926c6e9bc71483e6aab002c6a847a233
Alexey Alekhin <laughedelic@gmail.com>
2017-11-06 16:23:43 +0100

    Replaced blocking message-keeper with a scheduled timer

--- src/main/scala/ohnosequences/loquat/worker.scala
+++ src/main/scala/ohnosequences/loquat/worker.scala
@@ -4,16 +4,14 @@ import utils._, files._
 import ohnosequences.statika._
 import ohnosequences.datasets._
 import ohnosequences.awstools._, s3._, ec2._, regions._
-import com.amazonaws.services.s3.transfer.TransferManager
 import com.amazonaws.services.sqs.model.ReceiptHandleIsInvalidException
 import ohnosequences.awstools._, sqs._, s3._, ec2._
 
 import com.typesafe.scalalogging.LazyLogging
 import scala.concurrent._, duration._
 import scala.util, util.Try
-import java.util.concurrent.{ Executors, ExecutorService }
+import java.util.concurrent.ScheduledFuture
 
-import ExecutionContext.Implicits.global
 
 trait AnyWorkerBundle extends AnyBundle {
 
@@ -23,7 +21,7 @@ trait AnyWorkerBundle extends AnyBundle {
   type Config <: AnyLoquatConfig
   val  config: Config
 
-  val scheduler = Scheduler(2)
+  val scheduler = Scheduler(3)
 
   lazy val loggerBundle = LogUploaderBundle(config, scheduler)
 
@@ -32,12 +30,12 @@ trait AnyWorkerBundle extends AnyBundle {
     instructionsBundle
   )
 
-  lazy val ctx = GeneralContext(config, loggerBundle, instructionsBundle)
+  lazy val ctx = GeneralContext(config, loggerBundle, instructionsBundle, scheduler)
 
   def instructions: AnyInstructions = LazyTry {
     import ctx._
-    // TODO: any better way to loop this?
-    taskIteration.flatMap { _ => taskIteration }
+    // Await.result(taskLoop(ExecutionContext.Implicits.global), 12.hours)
+    taskLoop(ExecutionContext.Implicits.global)
   }
 }
 
@@ -57,7 +55,8 @@ abstract class WorkerBundle[
 case class GeneralContext(
   val config: AnyLoquatConfig,
   val loggerBundle: LogUploaderBundle,
-  val instructionsBundle: AnyDataProcessingBundle
+  val instructionsBundle: AnyDataProcessingBundle,
+  val scheduler: Scheduler
 ) extends LazyLogging { ctx =>
 
   lazy val workingDir: File = file("/media/ephemeral0/applicator/loquat")
@@ -84,37 +83,21 @@ case class GeneralContext(
     "artifact-url"     -> config.metadata.artifactUrl
   )
 
-  /* Execution context for the futures */
-  // TODO: probably download/upload futures need their own execution context (and AWS clients with more connectinos open)
-  // implicit lazy val globalContext = ExecutionContext.Implicits.global
-  // implicit lazy val fixedThreadPoolExecutionContext: ExecutionContext = {
-  //   val fixedThreadPool: ExecutorService = Executors.newFixedThreadPool(Runtime.getRuntime.availableProcessors * 6)
-  //   ExecutionContext.fromExecutor(fixedThreadPool)
-  //   // TODO: use publishError as a reporter
-  // }
-
-
   /* This is a _full_ iteration of the task processing loop */
-  def taskIteration(): Future[FiniteDuration] = {
+  def taskLoop(implicit ec: ExecutionContext): Future[FiniteDuration] = {
 
     receiveMessage.flatMap { message =>
-      val taskCtx = TaskContext(message, Deadline.now)(ctx)
-      val futureResult = taskCtx.processTask()
-
-      logger.info(s"Started task ${taskCtx.dataMapping.id}")
-
-      // TODO: can this cause a situation when all threads are ocupied by the futureResult's inner futures and there's no thread left for the keepMessageInFlight, which is supposed to run in parallel with futureResult?
-      Future.firstCompletedOf(Seq(
-        futureResult,
-        taskCtx.keepMessageInFlight(futureResult)
-      ))
-    }
+      TaskContext(
+        message,
+        Deadline.now
+      )(ctx).processTask
+    }.flatMap { _ => taskLoop }
   }
 
 
   /* Waits for a task-message from the input queue. It is supposed to wait and send requests as long as needed. */
   @SuppressWarnings(Array("org.wartremover.warts.TraversableOps"))
-  def receiveMessage(): Future[Message] = Future.fromTry {
+  def receiveMessage(implicit ec: ExecutionContext): Future[Message] = Future.fromTry {
     logger.info("Data processor is waiting for new data...")
 
     inputQueue.poll(
@@ -123,9 +106,6 @@ case class GeneralContext(
       adjustRequest = { _.withWaitTimeSeconds(10) }
     ).map { _.head }
   }
-  // NOTE: for whatever reason it fails, the best we can do is just try again
-  //.fallbackTo(receiveMessage())
-
 
 }
 
@@ -141,72 +121,83 @@ case class TaskContext(
 
   def timeSpent: FiniteDuration = -startTime.timeLeft
 
-
-  /* This is the main method that conbines all the other methods */
-  def processTask(): Future[FiniteDuration] = {
-
-    prepareInputData()
-      .flatMap(processFiles)
-      .flatMap(finishTask)
-      .map { _ =>
-        logger.info(s"Task ${dataMapping.id} took ${timeSpent.toSeconds}s")
-        timeSpent
+  val keeper: ScheduledFuture[_] = scheduler.repeat(
+    after = 1.second,
+    every = 7.seconds
+  ) {
+    logger.debug(s"Keeping message ${dataMapping.id} in flight (${timeSpent.toSeconds}s)")
+    message.changeVisibility(8.seconds).recover {
+      case ex: ReceiptHandleIsInvalidException => {
+        logger.warn(s"Task ${dataMapping.id} is already done by somebody")
+        keeper.cancel(true)
+        ()
       }
+    }
   }
 
+  /* This is the main method that conbines all the other methods */
+  def processTask(implicit ec: ExecutionContext): Future[FiniteDuration] = {
+    logger.info(s"Started task ${dataMapping.id}")
+
+    Future(taskPipeline).map { _ =>
+      keeper.cancel(true)
+      logger.info(s"Task ${dataMapping.id} took ${timeSpent.toSeconds}s")
+      timeSpent
+    }
+  }
+
+  def taskPipeline(): Unit = {
+    val input = prepareInputData
+    val output = processFiles(input)
+    finishTask(output)
+  }
 
   /* This method downloads one input data resource and returns the created file */
-  private def downloadInput(name: String, resource: AnyRemoteResource): Future[File] = resource match {
+  private def downloadInput(
+    name: String,
+    resource: AnyRemoteResource
+  ): File = resource match {
     /* - if the resource is a message, we just write it to a file */
-    case MessageResource(msg) => Future {
+    case MessageResource(msg) => {
       logger.debug(s"Input [${name}]: writing message to a file")
-      val f = (inputDir / name).overwrite(msg)
-      logger.debug(s"Input [${name}]: written to ${f.path}")
-      f
+      val file = (inputDir / name).overwrite(msg)
+      logger.debug(s"Input [${name}]: written to ${file.path}")
+      file
     }
     /* - if the resource is an S3 object/folder, we download it */
-    case S3Resource(s3Address) => Future.fromTry {
+    case S3Resource(s3Address) => {
       // NOTE: if it is an S3 folder, destination will be inputDir/name/<s3Address.key>/
       logger.debug(s"Input [${name}]: downloading [${s3Address}]")
       import ohnosequences.awstools.s3._
-      transferManager.download(
+      val file = transferManager.download(
         s3Address,
         inputDir / name
-      )
+      ).get
+      logger.debug(s"Input [${name}]: downloaded to [${file.path}]")
+      file
     }
   }
 
   /* This downloads input files for the given datamapping. Multiple files will be downloaded in parallel. */
-  def prepareInputData(): Future[Map[String, File]] = Future {
-
-    logger.debug(s"Cleaning up and preparing the working directory: ${workingDir.path}")
+  // def prepareInputData(implicit ec: ExecutionContext): Future[Map[String, File]] = {
+  def prepareInputData(): Map[String, File] = {
+    logger.debug(s"Cleaning up...")
     workingDir.deleteRecursively()
-    workingDir.createDirectory
-  }.map { _ =>
+    inputDir.createDirectory
+    outputDir.createDirectory
 
     logger.debug("Downloading input data...")
-    // FIXME: this parallel downloading gets stuck on MessageResource file-writing for some reason
     // Future.traverse(dataMapping.inputs) { case (name, resource) =>
-    //   downloadInput(name, resource).map { name -> _ }
-    // }.map { _.toMap }
-
-    // this doesn't work either:
-    // dataMapping.inputs.foldLeft(
-    //   Future.successful { Map[String, File]() }
-    // ) { case (accF, (name, resource)) =>
-    //   for {
-    //     acc <- accF
-    //     file <- downloadInput(name, resource)
-    //   } yield acc + (name -> file)
-    // }
-
+      // downloadInput(name, resource).map { name -> _ }
     dataMapping.inputs.map { case (name, resource) =>
-      name -> Await.result(downloadInput(name, resource), 5.minutes)
+      name -> downloadInput(name, resource)
     }
+    // }.map { _.toMap }
   }
 
-  def processFiles(inputFiles: Map[String, File]): Future[Map[String, File]] = Future {
-
+  def processFiles(
+    inputFiles: Map[String, File]
+  ): Map[String, File] = {
     logger.debug("Processing data...")
     instructionsBundle.runProcess(workingDir, inputFiles) match {
       case Success(_, outputFiles) => outputFiles
@@ -224,96 +215,47 @@ case class TaskContext(
   }
 
   /* This method uploads one output file and returns the destination S3 address or `None` if the files was empty and could be skept */
-  private def uploadOutput(file: File, destination: AnyS3Address): Future[Option[AnyS3Address]] = {
-
-    // FIXME: file.isEmpty may fail on compressed files!
-    if (config.skipEmptyResults && file.isEmpty) Future.successful {
+  private def uploadOutput(
+    file: File,
+    destination: AnyS3Address
+  ): Option[AnyS3Address] = {
+    if (config.skipEmptyResults && file.isEmpty) {
       logger.info(s"Output file [${file.getName}] is empty. Skipping it.")
       None
-    } else Future.fromTry {
-
+    } else {
       logger.info(s"Publishing output object: [${file.getName}]")
       transferManager.upload(
         file,
         destination,
         s3Metadata,
         true // silent
-      ).map { Some(_) }
+      ).toOption
     }
   }
 
   /* This method uploads all output files, sends a success-message and (!) deletes the task-message from the input queue */
-  def finishTask(outputFiles: Map[String, File]): Future[Unit] = {
+  // def finishTask(outputFiles: Map[String, File])(implicit ec: ExecutionContext): Future[Unit] = {
+  def finishTask(outputFiles: Map[String, File]): Unit = {
     // NOTE: this method could be split, but the point of it is to control that task is successfully finished _only_ if the output files are successfully uploaded. Then we can delete the task-message and the task is "announced" as done.
-    // logger.info("Uploading output files...")
-    Future.traverse(outputFiles) { case (name, file) =>
-
-      uploadOutput(file, dataMapping.outputs(name).resource)
-
-    }.flatMap { outputObjects =>
-
-      logger.info("Finished uploading output files. Publishing message to the output queue.")
-      Future.fromTry {
-        outputQueue.sendOne(
-          // TODO: attach normall log
-          ProcessingResult(instance.id, dataMapping.id).toString
-        )
+    logger.info("Uploading output files...")
+    // Future.traverse(outputFiles) { case (name, file) =>
+      outputFiles.map { case (name, file) =>
+        uploadOutput(file, dataMapping.outputs(name).resource)
       }
-    }.flatMap { _ =>
-      Future.fromTry { message.delete() }
-    }
+    // }.map { _ =>
+      notifyOutputQueue()
+    // }.map { _ =>
+      message.delete().get
+    // }
   }
 
-
-  /* This method is supposed to run in parallel with `processTask` to keep its message in-flight */
-  def keepMessageInFlight(futureResult: Future[FiniteDuration]): Future[FiniteDuration] = {
-    val deadline: Deadline =
-      config.terminationConfig
-        .taskProcessingTimeout // either configured timeout
-        .getOrElse(12.hours)   // or just the global one
-        .fromNow
-
-    val messageTimeout = inputQueue.visibilityTimeout
-
-    /* This future will be cycling until the task is done or timeout is exceeded */
-    Future {
-      // TODO: is there a better way to loop it?
-      while(!futureResult.isCompleted) {
-
-        /* We sleep a bit less to prolong visibility timeout _before_ the message will return to the queue */
-        // NOTE: probably this need some more careful "time-padding"
-        logger.debug(s"Going to sleep... zzzZzzZZZzzzz")
-        sleep(messageTimeout * 0.95)
-        logger.debug(s"Slept ${messageTimeout * 0.95}")
-
-        /* - if by the time we wake up everything is already done, no need to do anything else: we return the result (although it won't be used anywhere) */
-        if (futureResult.isCompleted) futureResult
-        /* - if it takes too much time we fail */
-        else if(deadline.isOverdue) {
-          // TODO: publish error message
-          suicide(new TimeoutException("Task processing timeout exceeded"))
-        /* - otherwise, data is still being processed and we try to keep the message in-flight */
-        } else {
-
-          logger.info(s"Keeping message ${dataMapping.id} in flight")
-          message.changeVisibility(messageTimeout).recover {
-
-            /* The message likely doesn't exist, i.e. already successfuly processed (possibly by somebody else) */
-            // NOTE: as we don't have any civilized way to stop ongoing parallel computation, we just terminate the instance
-            case ex: ReceiptHandleIsInvalidException =>
-              suicide(new ReceiptHandleIsInvalidException(s"Task ${dataMapping.id} is already done by somebody"))
-
-            /* The message exists, but is not in flight, i.e. at some point we failed to keep it in-flight */
-            // NOTE: we can ignore this case because once the task will be finished (by this or another instance) it will be deleted and the other case will terminate the slowpoke worker
-            // case ex: MessageNotInflightException =>
-          }
-        }
-      }
-
-      timeSpent
-    }
+  def notifyOutputQueue(): Unit = {
+    logger.info("Finished uploading output files. Publishing message to the output queue.")
+    outputQueue.sendOne(
+      // TODO: attach normall log
+      ProcessingResult(instance.id, dataMapping.id).toString
+    ).get
   }
-
 
   /* Publishes an error message to the SQS queue */
   def publishError(msg: String): Try[MessageId] = {
